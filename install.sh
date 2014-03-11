7bd5c27a9efdd9026bb193bc1dd348d47eeeaf42
Shichao An <shichao.an@nyu.edu>
2014-03-10 22:52:08 -0400

    First commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# usage: ./install.sh yum cpan git
+# Install dependencies and make symbolic links from the Git repository
+while (( "$#" ))
+do
+    if [ "$1" = "yum" ]
+    then
+        yum -y install perl-libxml-perl perl-WWW-Mechanize \
+            perl-Compress-Raw-Zlib perl-Crypt-Rijndael ffmpeg
+        shift
+    elif [ "$1" = "cpan" ]
+    then
+        cpan install local::lib WWW::Mechanize Module::Find LWP::Protocol::https
+        shift
+    elif [ "$1" = "git" ]
+    then
+        git clone https://github.com/monsieurvideo/get-flash-videos.git
+        ln -s get-flash-videos/get_flash_videos \
+            /usr/local/bin/get_flash_videos
+        shift
+    fi
+done
